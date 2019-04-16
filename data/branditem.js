2d2b7e87cb05fe1886dd03dd20acd9cb69351303
thachpn03tcdb@gmail.com <Ik29250708>
2019-04-16 22:20:34 +0700

    edit
    edit

---
+++ data/branditem.js
@@ -0,0 +1,39 @@
+var branditem = [
+  {
+    "key": "1",
+    "name": "Phúc Long Lê Văn Sỹ",
+    "address" : "350 Đường Lê Văn Sĩ,phường 14,...",
+    "detail" : "Quán kinh doang các loại cà phê, matcha,... có không gian thoải mái, yên tĩnh, phù hợp cho học tập và làm việc",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6QTlD9E-WI-qg988aDozGn07sYSRkgZnobWeWum1RyJKE3bfg"
+  },
+  {
+    "key": "2",
+    "name": "Phúc Long Cộng Hòa",
+    "address" : "250 Đường Cộng Hòa, phường 11,...",
+    "detail": "",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzve1X1_258WZehjfr9Wj78TwwQ_ZZm60b-xG1E_aPImTVcqaxMA"
+  },
+  {
+    "key": "3",
+    "name": "Phúc Long quận 1",
+    "address" : "150 Đường Lê Lợi, phường Bến Nghé,...",
+    "detail" : "",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMUfDQaEdPMGdGo4Z4NutX1cUlZgFhbo2VUGtYbOfcnXP7PMr1LQ"
+  },
+  {
+    "key": "4",
+    "name": "Phúc Long quận 3",
+    "address" : "150 Đường Lê Văn Sĩ, phường 2,...",
+    "detail" : "",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTivaNIhp1txd7K8dcMhtw30sRKZjzNdvClYvupoE1Magq1zFT2iA"
+  },
+  {
+    "key": "5",
+    "name": "Phúc Long quận 5",
+    "address" : "50 Đường Nguyễn Chí Thanh, phường 7,...",
+    "detail" : "",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtBztUWR92K2Wo9bt-tP9tJzWr28Vnm0I7wziCbGrcSoxjmi06"
+  }
+];
+
+module.exports = branditem;
--- index.js
+++ index.js
@@ -7,5 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import ListViewDemo from './ListViewDemo'
 import BasicListItem from './component/BasicListItem'
+import BrandOneItem from './component/BrandOneItem'
+import FavoritePlace from './component/FavoritePlace'
 
-AppRegistry.registerComponent(appName, () => BasicListItem);
+AppRegistry.registerComponent(appName, () => FavoritePlace);
